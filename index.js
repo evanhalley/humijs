e9f6f9263627857def44b58c21c538dbb25d7a0a
Evan Halley <evan.halley@gmail.com>
2020-11-03 03:03:37 +0000

    adding source files

---
+++ index.js
@@ -0,0 +1,27 @@
+const sensor = require("node-dht-sensor").promises;
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('humijs online');
+});
+
+app.get('/report', async (req, res) => {
+	try {
+    		const data = await sensor.read(22, 4);
+		const response = {
+			temperature: data.temperature.toFixed(2),
+			humidity: data.humidity.toFixed(2)
+		}
+		res.status(200).json(response);
+	} catch (err) {
+		console.log('Error reading sensor', err);
+		res.status(500).json({ error: 'Error reading sensor: ' + err });
+   	}
+});
+
+app.listen(port, () => {
+  	console.log(`Example app listening at http://localhost:${port}`)
+})
